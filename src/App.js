109888064168953729e68e5145ae75147a06c957
Yusuf <yakande96@gmail.com>
2021-01-18 13:02:08 +0100

    final finale

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
-import { v4 as uuid } from 'uuid';
-// why will unused variable cause build failure
+import React, { Component } from "react";
+import uuid from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
-
-export default class App extends Component {
+class App extends Component {
   state = {
     items: [],
     id: uuid(),
@@ -25,47 +23,57 @@ export default class App extends Component {
     };
     const updatedItems = [...this.state.items, newItem];
 
-    this.setState(
-      {
-        items: updatedItems,
-        item: "",
-        id: uuid(),
-        editItem: false
-      },
-      () => console.log(this.state)
-    );
+    this.setState({
+      items: updatedItems,
+      item: "",
+      id: uuid(),
+      editItem: false
+    });
   };
   clearList = () => {
-    console.log("clear list ");
+    this.setState({
+      items: []
+    });
   };
   handleDelete = id => {
-    console.log(`handle edit ${id}`);
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    this.setState({
+      items: filteredItems
+    });
   };
   handleEdit = id => {
-    console.log(`edit edit ${id}`);
+    const filteredItems = this.state.items.filter(item => item.id !== id);
+    const selectedItem = this.state.items.find(item => item.id === id);
+    this.setState({
+      items: filteredItems,
+      item: selectedItem.title,
+      id: id,
+      editItem: true
+    });
   };
-  
-
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-10 mx-auto col-md-8 mt-5">
-            <h3 className="text-capitalize text-centre">todo input</h3>
-            <TodoInput  
+            <h3 className="text-capitalize text-center">todo input</h3>
+            <TodoInput
               item={this.state.item}
               handleChange={this.handleChange}
               handleSubmit={this.handleSubmit}
-              editItem={this.state.editItem}></TodoInput>
-            <TodoList 
+              editItem={this.state.editItem}
+            />
+            <TodoList
               items={this.state.items}
               clearList={this.clearList}
               handleDelete={this.handleDelete}
-              handleEdit={this.handleEdit}></TodoList>
+              handleEdit={this.handleEdit}
+            />
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
+export default App;
